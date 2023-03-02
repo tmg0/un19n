8b26f1d82511ec3f0de47edafcb85da68af3508b
zekunjin <zekun.jin@foxmail.com>
2023-03-02 15:07:59 +0800

    chore: use unplugin

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
-export * from './src/shared/common'
-export * from './src/core/translators/baidu'
+
+import { createUnplugin } from 'unplugin'
+
+export { translators } from './src/core'
+
+export const translationFuncRE = /\st\({zh|en}:(.*)\s*?/
+
+export const un19n = createUnplugin(() => {
+  return {
+    name: 'un19n',
+    transformInclude: id => /.*src.*\.(ts|js|tsx|jsx|vue)/.test(id),
+    transform: () => {
+      return {
+        code: '',
+        map: ''
+      }
+    }
+  }
+})
--- src/core/index.ts
+++ src/core/index.ts
@@ -3,6 +3,6 @@ import { readUn19nConfig } from 'src/shared/common'
 
 const conf = readUn19nConfig()
 
-export const translator: Record<Platform, Translator> = {
+export const translators: Record<Platform, Translator> = {
   baidu: baiduTranslator(await conf)
 }
