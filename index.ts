499122b2747b9fa3fb4518649b00e6d160bb9601
zekunjin <zekun.jin@foxmail.com>
2023-03-02 15:30:41 +0800

    chore: return default code

--- index.ts
+++ index.ts
@@ -3,17 +3,14 @@ import { createUnplugin } from 'unplugin'
 
 export { translators } from './src/core'
 
-export const translationFuncRE = /\st\({zh|en}:(.*)\s*?/
+export const keywordRE = /\st\((zh|en):(.*)\s*?/
 
 export const un19n = createUnplugin(() => {
   return {
     name: 'un19n',
     transformInclude: id => /.*src.*\.(ts|js|tsx|jsx|vue)/.test(id),
-    transform: () => {
-      return {
-        code: '',
-        map: ''
-      }
+    transform: (code) => {
+      return { code }
     }
   }
 })
