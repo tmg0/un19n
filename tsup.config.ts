8de6e9740e45d06e988bbfaea1fa9bf71ab134a2
zekunjin <zekun.jin@foxmail.com>
2023-03-06 14:57:11 +0800

    build: rm source map conf

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig(options => ({
   entry: ['./src/index.ts'],
   splitting: true,
-  sourcemap: true,
   clean: true,
   treeshake: true,
   dts: true,
