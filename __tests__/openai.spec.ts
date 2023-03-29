d562f67c78df03f2f1fabe8d7e1501a2ddde90b3
zekunjin <zekun.jin@foxmail.com>
2023-03-29 16:05:44 +0800

    feat: add openai translation

--- __tests__/openai.spec.ts
+++ __tests__/openai.spec.ts
@@ -5,6 +5,6 @@ import { translate } from '../src/core'
 describe('openai translator', () => {
   it('should translate message by openai open api', async () => {
     const conf = await readUn19nConfig()
-    expect(await translate(conf)(['apple'], conf.from, conf.to)).toBe('苹果')
+    expect((await translate(conf)(['apple', 'orange'], conf.from, 'zh'))[0]).toBe('苹果')
   }, 60 * 60 * 1000)
 })
--- src/core/translators/openai.ts
+++ src/core/translators/openai.ts
@@ -17,46 +17,49 @@ export interface OpenaiCompletion {
   stop: string | null
 }
 
+export interface OpenaiCompletionChoice {
+  finish_reason: string
+  index: string
+  logprobs?: unknown
+  text: string
+}
+
+export interface OpenaiCompletionResult {
+  choices: OpenaiCompletionChoice[]
+}
+
 export const openaiTranslator = ({ apiKey, organization, proxy }: Un19nConfig): Translator => async (messages, from, to) => {
   if (!apiKey) { return '' }
   if (!organization) { return '' }
 
   const multi = isArray(messages)
+  const msgs = multi ? messages : [messages]
   const baseURL = BaseURL.OPENAI
+
+  const params = {
+    model: 'text-davinci-003',
+    prompt: msgs.map(msg => `Translate ${msg} to ${to}`),
+    max_tokens: 60,
+    temperature: 0,
+    top_p: 1,
+    n: 1,
+    stream: false,
+    logprobs: null,
+    stop: null
+  }
 
   const options = {
     method: 'POST',
     baseURL,
-    agent: HTTP_PROXY_AGENT,
     headers: { Authorization: `Bearer ${apiKey}` },
-    body: JSON.stringify({
-      model: 'text-davinci-002',
-      prompt: 'Say this is a test',
-      max_tokens: 7,
-      temperature: 0,
-      top_p: 1,
-      n: 1,
-      stream: false,
-      logprobs: null,
-      stop: null
-    })
+    body: JSON.stringify(params)
   } as any
 
   if (proxy) { options.agent = new HttpsProxyAgent(proxy) }
 
-  const response = await ofetch(OPENAI_COMPLETIONS, options)
+  const response = await ofetch<OpenaiCompletionResult>(OPENAI_COMPLETIONS, options)
 
-  console.log(response)
+  const result = response.choices.map(({ text }) => text.trim())
 
-  // const response = await openai.createCompletion({
-  //   model: 'text-davinci-003',
-  //   prompt: 'Say this is a test',
-  //   max_tokens: 7,
-  //   temperature: 0,
-  //   stop: null
-  // })
-
-  // console.log(response)
-
-  return ''
+  return multi ? result : result[0] || ''
 }
