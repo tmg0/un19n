4c6768206e04a010f9a8adc502a58d3b7dcc6ec9
zekunjin <zekun.jin@foxmail.com>
2024-02-27 15:43:54 +0800

    chore: add new options type

--- src/types.ts
+++ src/types.ts
@@ -4,8 +4,33 @@ export type Platform = 'baidu' | 'openai'
 
 export type Translator = <T extends string | string[]>(message: T[], from: Language, to: Language) => Promise<T[]>
 
+export interface BaiduOptions {
+  appid: string
+  secret: string
+}
+
 export interface Un19nOptions {
+  /**
+   * Translator based platform
+   * @default 'baidu'
+   */
   platform: Platform
+
+  /**
+   * Default source language
+   * @default 'en'
+   */
   from: Language
+
+  /**
+   * Translation target languages
+   * @default []
+   */
   to: Language[]
+
+  /**
+   * Baidu APIs access options
+   * @default undefined
+   */
+  baidu?: BaiduOptions
 }
