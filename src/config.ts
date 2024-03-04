a04fe56e212f2a2adcac2dd7d449a62fd61910cf
tmg0 <zekun.jin@foxmail.com>
2024-03-04 14:12:34 +0800

    fix: type err

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 import { loadConfig } from 'c12'
-import { defu } from 'defu'
 import { DEFAULT_UN19N_OPTIONS, UN19N_CONFIG_FILE } from './constants'
 import { Un19nOptions } from './types'
-import { Un19nPluginOptions } from './_unplugin'
 
 export const defineConfig = (options: Un19nOptions) => options
 
@@ -10,9 +8,4 @@ export const resolveConfig = async () => {
   const { config } = await loadConfig<Un19nOptions>({ name: UN19N_CONFIG_FILE, cwd: process.cwd(), defaults: DEFAULT_UN19N_OPTIONS })
   if (!config) { throw new Error('Can not find un19n config file.') }
   return config
-}
-
-export const resolvePluginConfig = async (options: Partial<Un19nPluginOptions>) => {
-  const config = await resolveConfig()
-  return defu(options as Un19nPluginOptions, config)
 }
